51a3ae2f1aa04a047c6b33a915422f186e8b5786
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-06-27 11:08:06 -0400

    finished sinatra hello world basics lab

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
+require_relative 'config/environment'
+
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+end
 
   # Define your GET '/' route below and respond with "Hello, World!"
 
 
-end
